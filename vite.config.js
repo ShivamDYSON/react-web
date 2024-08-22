c80ab2ade4ed673f36c471f3e775348a2ee156ff
ShivamDYSON <shivamprakash2004@gmail.com>
2024-08-22 01:42:58 -0700

    hg

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/react-web/', // Set this to your repository name
+  base: '/react-web/', // Ensure this matches your GitHub repository name
   plugins: [react()],
 });
